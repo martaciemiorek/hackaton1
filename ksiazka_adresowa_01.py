991eb6dc3e5277c04d152de221b9e94afd43bfa6
unknown <martaciemiorek@gmail.com>
2023-05-20 14:36:50 +0200

    zadanie

---
+++ ksiazka_adresowa_01.py
@@ -0,0 +1,11 @@
+address_book = {
+    'first_name' : ['Jan', 'Anna', 'Krzysztof', 'Małgorzata', 'Piotr', 'Alicja', 'Wojciech', 'Zofia', 'Michał', 'Barbara'],
+    'last_name' : ['Kowalski', 'Nowak', 'Wiśniewski', 'Wójcik', 'Kowalczyk', 'Kamińska', 'Lewandowski', 'Zielińska', 'Szymański', 'Woźniak'],
+    'phone' : ['501234567', '512345678', '523456789', '534567890', '545678901', '556789012', '567890123', '578901234', '589012345', '590123456'],
+    'department' : ['accounting', 'accounting', 'HR', 'HR', 'HR', 'HR', 'engineering','engineering', 'engineering', 'engineering'],
+    'email' : []
+    }
+
+print(address_book)
+
+print(address_book['first_name'][1])
